c9afdc8a179732f13af02e15ffa371c4ac7aec94
joshua.amigogo <joshua.amigogo@gmail.com>
2023-03-26 16:32:58 +0100

    source change

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://ai-bot-backend.onrender.com:10000",
+        target: "https://ai-bot-backend.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
