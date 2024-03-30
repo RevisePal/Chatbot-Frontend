f94cf155d6a360860b816b337886e1679b3ea7fc
revisepal-paolo <121865352+revisepal-paolo@users.noreply.github.com>
2024-03-30 14:18:09 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function App() {
         </div>
       </div>
       {/* Input box fixed at the bottom */}
-      <div className="fixed bottom-5 left-5 right-5 p-4 bg-white shadow-lg">
+      <div className="fixed bottom-5 left-5 right-5 p-4 bg-transparent shadow-lg">
   <div className="flex rounded-lg items-center border border-gray-300">
     <input
       type="text"
