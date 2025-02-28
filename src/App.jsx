c66cf004c5b930da3723fb7e377c4133645e838e
Paolo Farina <paolo.revisepal@gmail.com>
2025-02-28 17:28:26 +0000

    Math symbols fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,193 +1,169 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import send from "./assets/send.png";
 import robot from "./assets/robot.png";
 import student from "./assets/student.jpg";
 import loadingGif from "./assets/loading.gif";
-// import { url } from './utils/utils';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function App() {
   const [prompt, updatePrompt] = useState("");
   const [loading, setLoading] = useState(false);
-  const [copied, setCopied] = useState(null); // null indicates nothing is copied
+  const [copied, setCopied] = useState(null);
   const [conversation, setConversation] = useState([]);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const question = urlParams.get("question");
-    updatePrompt(question || ""); // Use empty string if question is null
+    updatePrompt(question || "");
   }, []);
 
+  useEffect(() => {
+    const chatContainer = document.getElementById("chat-container");
+    if (chatContainer) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [conversation]);
+
   const handleCopyClick = (text, index) => {
+    if (!text) return;
     navigator.clipboard.writeText(text);
-    setCopied(index); // Pass the index of the copied message
-    setTimeout(() => setCopied(null), 1000); // Reset the copied state after 1 second
+    setCopied(index);
+    setTimeout(() => setCopied(null), 1000);
   };
 
-  function stripHtmlTags(html) {
+  const stripHtmlTags = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent || "";
-  }
+  };
 
-  const sendPrompt = async (event) => {
-    if (event.key !== "Enter" || prompt.trim() === "") {
-      return;
-    }
+  const sendPrompt = async () => {
+    if (loading || prompt.trim() === "") return;
 
-    // Prepare the conversation history for the backend
-    // Assuming the backend expects an array of {role, content} objects
-    const conversations = conversation
-      .map((conv) => ({
-        role: "user",
-        content: conv.prompt,
-      }))
-      .concat({
-        role: "assistant",
-        content: prompt, // The current prompt being sent
-      });
-
+    setLoading(true);
     try {
-      setLoading(true);
+      const conversations = conversation
+        .map((conv) => ({
+          role: "user",
+          content: conv.prompt,
+        }))
+        .concat({
+          role: "user",
+          content: prompt,
+        });
 
-      const requestOptions = {
+      const res = await fetch("https://ai-bot-backend.onrender.com/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ conversations }),
-      };
+      });
 
-      const res = await fetch(
-        `https://ai-bot-backend.onrender.com/ask`,
-        requestOptions
-      );
-
-      if (!res.ok) {
-        throw new Error("Something went wrong");
-      }
+      if (!res.ok) throw new Error("Something went wrong");
 
       const { message } = await res.json();
       setConversation([...conversation, { prompt, response: message }]);
       updatePrompt("");
     } catch (err) {
-      console.error(err, "err");
+      console.error(err);
     } finally {
       setLoading(false);
     }
   };
 
-  const handleIconClick = () => {
-    if (!loading) {
-      sendPrompt({ key: "Enter" });
-    }
-  };
-
   return (
-    <div className="flex flex-col items-center justify-start w-screen pt-10 text-center overflow-auto min-h-screen bg-gradient-to-b from-blue-700 to-blue-400">
-      <h1 className="text-white w-full text-4xl font-sans">
-        Welcome to TutorGPT
-      </h1>
-      <h2 className="text-white w-full text-base font-light font-sans">
-        Powered by GPT-4
-      </h2>
-      {/* Use flex-grow to make this div take up all available space, pushing the input box to the bottom */}
-      <div
-        className="flex-grow w-10/12 flex flex-col items-center justify-end"
-        style={{ paddingBottom: "6rem" }}
-      >
+    <MathJaxContext>
+      <div className="flex flex-col items-center justify-start w-screen pt-10 text-center overflow-auto min-h-screen bg-gradient-to-b from-blue-700 to-blue-400">
+        <h1 className="text-white w-full text-4xl font-sans">
+          Welcome to TutorGPT
+        </h1>
+        <h2 className="text-white w-full text-base font-light font-sans">
+          Powered by GPT-4o
+        </h2>
         <div
-          className="w-full flex flex-col-reverse overflow-y-auto bg-transparent"
-          style={{ maxHeight: "calc(100vh - 12rem)" }}
+          className="flex-grow w-10/12 flex flex-col items-center justify-end"
+          style={{ paddingBottom: "6rem" }}
         >
-          {conversation
-            .slice()
-            .reverse()
-            .map((item, index) => (
-              <div key={index}>
-                <div
-                  className="message user-message"
-                  style={{ marginBottom: "1rem", textAlign: "left" }}
-                >
-                  <div className="avatar-and-text flex flex-row">
-                    <img
-                      className="bot-avatar"
-                      src={student}
-                      alt="robot avatar"
-                    />
-                    <p
-                      style={{
-                        color: "#333",
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "1rem",
-                        lineHeight: "1.5",
-                        fontWeight: "700",
-                      }}
-                    >
-                      {stripHtmlTags(item.prompt)}
-                    </p>
+          <div
+            id="chat-container"
+            className="w-full flex flex-col-reverse overflow-y-auto"
+            style={{ maxHeight: "calc(100vh - 12rem)" }}
+          >
+            {conversation
+              .slice()
+              .reverse()
+              .map((item, index) => (
+                <div key={index}>
+                  <div
+                    className="message user-message"
+                    style={{ marginBottom: "1rem", textAlign: "left" }}
+                  >
+                    <div className="avatar-and-text flex flex-row">
+                      <img
+                        className="bot-avatar"
+                        src={student}
+                        alt="user avatar"
+                      />
+                      <p className="text-gray-900 font-semibold">
+                        {stripHtmlTags(item.prompt)}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="message bot-message">
+                    <div className="avatar-and-text flex flex-row">
+                      <img
+                        className="bot-avatar"
+                        src={robot}
+                        alt="robot avatar"
+                      />
+                      <p>
+                        {index === 0 && loading ? (
+                          <img
+                            style={{ height: "20px", marginLeft: "10px" }}
+                            src={loadingGif}
+                            alt="loading"
+                          />
+                        ) : (
+                          <MathJax>{`${item.response}`}</MathJax>
+                        )}
+                      </p>
+                    </div>
+                    {item.response && (
+                      <FontAwesomeIcon
+                        className="hover:cursor-pointer p-2 hover:bg-gray-300"
+                        icon={copied === index ? faCheck : faCopy}
+                        onClick={() => handleCopyClick(item.response, index)}
+                      />
+                    )}
                   </div>
                 </div>
-
-                <div className="message bot-message">
-                  <div className="avatar-and-text flex flex-row">
-                    <img
-                      className="bot-avatar"
-                      src={robot}
-                      alt="robot avatar"
-                    />
-                    <p>
-                      {index === 0 && loading ? (
-                        <img
-                          style={{ height: "20px", marginLeft: "10px" }}
-                          src={loadingGif}
-                          alt="loading"
-                        />
-                      ) : (
-                        item.response
-                      )}
-                    </p>
-                  </div>
-                  {copied === index ? (
-                    <FontAwesomeIcon
-                      className="hover: bg-gray-300"
-                      icon={faCheck}
-                      onClick={() => handleCopyClick(item.response, index)}
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      className="hover:cursor-pointer p-2 hover:bg-gray-300"
-                      icon={faCopy}
-                      onClick={() => handleCopyClick(item.response, index)}
-                    />
-                  )}
-                </div>
-              </div>
-            ))}
+              ))}
+          </div>
+        </div>
+        <div className="fixed bottom-0 left-5 right-5 p-4 bg-transparent mt-4">
+          <div className="relative rounded-lg">
+            <textarea
+              className="flex-grow rounded-lg pl-2.5 pr-12 py-2 focus:outline-none text-sm md:text-base w-full min-h-15 resize-none"
+              placeholder="Ask TutorGPT..."
+              disabled={loading}
+              value={stripHtmlTags(prompt)}
+              onChange={(e) => updatePrompt(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") sendPrompt();
+              }}
+              style={{ paddingRight: "2rem" }}
+            />
+            <img
+              className="absolute right-2 bottom-6 w-8 h-8 cursor-pointer bg-transparent"
+              src={send}
+              alt="send icon"
+              onClick={sendPrompt}
+            />
+          </div>
         </div>
       </div>
-      {/* Input box fixed at the bottom */}
-      <div className="fixed bottom-0 left-5 right-5 p-4 bg-transparent mt-4">
-        <div className="relative rounded-lg ">
-          <textarea
-            className="flex-grow rounded-lg pl-2.5 pr-12 py-2 focus:outline-none text-sm md:text-base w-full min-h-15 resize-none"
-            placeholder="Ask TutorGTP..."
-            disabled={loading}
-            value={stripHtmlTags(prompt)}
-            onChange={(e) => updatePrompt(e.target.value)}
-            onKeyDown={(e) => sendPrompt(e)}
-            style={{ paddingRight: "2rem" }}
-          />
-
-          <img
-            className="absolute right-2 bottom-6 w-8 h-8 cursor-pointer bg-transparent"
-            src={send}
-            alt="send icon"
-            onClick={handleIconClick}
-          />
-        </div>
-      </div>
-    </div>
+    </MathJaxContext>
   );
 }
 
